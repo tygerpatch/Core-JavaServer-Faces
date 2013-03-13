86cb0065293f45a4678061c1e7fae5d5f7c5d260
Todd Gerspacher <toddgerspacher27@gmail.com>
2013-03-13 11:21:54 -0400

    * removed methods that aren't called * program to an interface, not an implementation

--- src/Chapter/Two/ProblemBean.java
+++ src/Chapter/Two/ProblemBean.java
@@ -1,37 +1,30 @@
 package Chapter.Two;
 
-import java.util.ArrayList;
-
 public class ProblemBean {
-
-  private ArrayList<Integer> sequence;
+  private int[] sequence;
   private int solution;
 
   public ProblemBean() {
   }
 
-  public ProblemBean(int[] values, int solution) {
-    sequence = new ArrayList<Integer>();
-    for (int i = 0; i < values.length; i++) {
-      sequence.add(values[i]);
-    }
+  public ProblemBean(int[] sequence, int solution) {
+    this.sequence = sequence;
     this.solution = solution;
   }
 
-  public ArrayList<Integer> getSequence() {
-    return sequence;
-  }
+  public String getSequence() {
+    StringBuilder builder = new StringBuilder();
 
-  public void setSequence(ArrayList<Integer> newValue) {
-    sequence = newValue;
-  }
+    for(int i = 0; i < (sequence.length - 1); i++) {
+      builder.append(sequence[i]);
+      builder.append(", ");
+    }
+    builder.append(sequence[sequence.length - 1]);
 
+    return builder.toString();
+  }
 
   public int getSolution() {
     return solution;
-  }
-
-  public void setSolution(int newValue) {
-    solution = newValue;
   }
 }
--- src/Chapter/Two/QuizBean.java
+++ src/Chapter/Two/QuizBean.java
@@ -1,49 +1,42 @@
 package Chapter.Two;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class QuizBean {
-  private ArrayList<ProblemBean> problems = new ArrayList<ProblemBean>();
+  private List<ProblemBean> problems = new ArrayList<ProblemBean>();
   private int currentIndex;
   private int score;
 
   public QuizBean() {
-    problems.add(new ProblemBean(new int[] { 3, 1, 4, 1, 5 }, 9));    // pi
-    problems.add(new ProblemBean(new int[] { 1, 1, 2, 3, 5 }, 8));    // fibonacci
-    problems.add(new ProblemBean(new int[] { 1, 4, 9, 16, 25 }, 36)); // squares
-    problems.add(new ProblemBean(new int[] { 2, 3, 5, 7, 11 }, 13));  // primes
-    problems.add(new ProblemBean(new int[] { 1, 2, 4, 8, 16 }, 32));  // powers of 2
+    problems.add(new ProblemBean(new int[] {3, 1, 4, 1, 5}, 9));    // pi
+    problems.add(new ProblemBean(new int[] {1, 1, 2, 3, 5}, 8));    // fibonacci
+    problems.add(new ProblemBean(new int[] {1, 4, 9, 16, 25}, 36)); // squares
+    problems.add(new ProblemBean(new int[] {2, 3, 5, 7, 11}, 13));  // primes
+    problems.add(new ProblemBean(new int[] {1, 2, 4, 8, 16}, 32));  // powers of 2
   }
 
-  // PROPERTY: problems
-  public void setProblems(ArrayList<ProblemBean> newValue) {
-    problems = newValue;
-    currentIndex = 0;
-    score = 0;
-  }
-
-  // PROPERTY: score
   public int getScore() {
     return score;
   }
 
-  // PROPERTY: current
   public ProblemBean getCurrent() {
     return problems.get(currentIndex);
   }
 
-  // PROPERTY: answer
   public String getAnswer() {
     return "";
   }
 
-  public void setAnswer(String newValue) {
+  public void setAnswer(String strAnswer) {
     try {
-      int answer = Integer.parseInt(newValue.trim());
+      int answer = Integer.parseInt(strAnswer.trim());
+
       if (getCurrent().getSolution() == answer) {
         score++;
       }
-      currentIndex = (currentIndex + 1) % problems.size();
+
+      currentIndex = (currentIndex + 1) % problems.size();  // causes program to loop back to first question
     }
     catch (NumberFormatException ex) {
     }
